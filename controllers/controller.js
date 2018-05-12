91c841730ec67c6c2a1731340d2b663f75207cef
AL <alice.lin96@gmail.com>
2018-05-12 17:54:21 +1000

    neatened controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,27 +2,24 @@ const html_dir = '/../public/html';
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+const request = require('request');
+
 
 var User = require('../models/user.js');
 var Food = require('../models/food.js');
 var SavedRecipe = require('../models/saved_recipes.js');
 
 
-
-const request = require('request');
-
 const resultsPerPage = 24;
 
 module.exports = {
-    displayPage : function(req, res){
-        res.sendFile('welcomepage.html', {root: __dirname + html_dir});
-        },
 
+    /*Displays food tracker*******************************************************************************************/
     displayFoodPage : function(req, res){
         res.sendFile('foodtracker.html', {root: __dirname + html_dir});
     },
 
-    //Can reuse this for the saved recipes part
+    /*Displays Recipe results after search ***************************************************************************/
     displayRecipes: function (req, res) {
         var page = 1;
         if(req.query.page) {
@@ -68,8 +65,6 @@ module.exports = {
             }
         });
 
-       // console.log(favouritesIDArray);
-
         // Make the request to the api
         request.get({
                 uri: "https://api.edamam.com/search",
@@ -89,7 +84,7 @@ module.exports = {
                         favourites: favouritesIDArray
                     });
                 }
-                /* //this isn't printing for some reason
+                /*
                  console.log(response.statusCode, body);
                  res.render("results", {data: JSON.parse(body)});
              } else {
@@ -104,105 +99,7 @@ module.exports = {
 
 };
 
-// module.exports = {
-//     registerUser: function (req, res) {
-//         var username = req.body.username;
-//         var name = req.body.name;
-//         var email = req.body.email;
-//         var password = req.body.password;
-//
-//         var newuser = new User();
-//         newuser.username = username;
-//         newuser.name = name;
-//         newuser.email = email;
-//         newuser.password = password;
-//
-//         newuser.save(function (err, savedUser) {
-//             if (err) {
-//                 console.log(err);
-//                 return status(500).send();
-//             }
-//             return res.status(200).send();
-//         });
-//
-//     }
-// };
-
-
-
-// from lecture
-// //
-// module.exports = {
-//     registerUser: function (req, res) {
-//         var user = new User({
-//             "username": req.body.username,
-//             "name": req.body.name,
-//             "email": req.body.email,
-//             "password": req.body.password
-//         });
-//         user.save(function (err, newUser) {
-//             if (!err) {
-//                 res.send(newUser);
-//             } else {
-//                 res.sendStatus(400);
-//             }
-//         });
-//     }
-// };
-
-
-//
-var registerUser = function(req, res){
-    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
-    var user = new User({
-        "username": req.body.username,
-        "name": req.body.name,
-        "email": req.body.email,
-        "password": hashedPassword
-    });
-    user.save(function(err,newUser){
-        if(!err){
-            res.send(newUser);
-        } else{
-            res.sendStatus(400);
-        }
-    })
-};
-
-var login = function(req, res){
-    console.log(req.body);
-    const username = req.body.username;
-    User.findOne({username: username}, username, (err, user) => {
-        if (!err) {
-            console.log(user);
-            if (bcrypt.compareSync(req.body.password, user.password)) {
-                const token = jwt.sign({ id: user.username }, process.env.SECRET, {
-                    expiresIn: 86400 // expires in 24 hours
-                });
-                res.status(200).send({auth: true, token: token});
-                console.log("worked")
-            } else {
-                res.sendStatus(401);
-                console.log("didn't work");
-            }
-        } else {
-            res.sendStatus(404);
-            console.log("couldn't find user")
-        }
-    });
-};
-
-
-var findAllUsers = function(req,res){
-    User.find(function(err,users){
-        if(!err){
-            res.send(users);
-        }else{
-            res.sendStatus(404);
-        }
-    });
-};
-
+/*Finds the user which is currently logged in ************************************************************************/
 var findOneUser = function(req,res){
     var username = req.user.username;
     User.find({username: username}, function(err, user){
@@ -214,6 +111,7 @@ var findOneUser = function(req,res){
     });
 };
 
+/*Find the food items the user has saved in the food tracker**********************************************************/
 var findUserFood = function(req, res) {
     const username = req.user.username;
     Food.find({username: username}, (err, foods) => {
@@ -226,7 +124,7 @@ var findUserFood = function(req, res) {
     })
 };
 
-
+/*Add a food to the food tracker database*****************************************************************************/
 var registerFood = function(req, res){
     console.log(req.body);
     var food = new Food({
@@ -245,6 +143,7 @@ var registerFood = function(req, res){
     })
 };
 
+/*Delete a food item from the  food tracker database******************************************************************/
 var deleteFood = function(req, res) {
     console.log(req.body);
     Food.findByIdAndRemove(req.body._id, function(err, newFood){
@@ -256,11 +155,8 @@ var deleteFood = function(req, res) {
     })
 
 };
-//
-module.exports.registerUser = registerUser;
-module.exports.findAllUsers = findAllUsers;
+
 module.exports.findOneUser = findOneUser;
-module.exports.login = login;
 module.exports.findUserFood = findUserFood;
 module.exports.registerFood = registerFood;
 module.exports.deleteFood = deleteFood;
