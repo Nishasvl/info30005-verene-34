1f415c50b6b9f0014a0d38be94193e3f05df5425
AL <alice.lin96@gmail.com>
2018-05-27 14:52:17 +1000

    link to register works

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,4 +1,18 @@
+const registerTab="register"
+const loginTab="login"
+
 $(function() {
+    $(function(){
+        var url_string = window.location.href;
+        var tab = url_string.split("#");
+        if(tab.includes(registerTab)){
+            $("#register-form").show();
+            $("#login-form").hide();
+
+            $('#login-form-link').removeClass('active');
+            $('#register-form-link').addClass('active');
+        }
+    });
 
     // passport log in function
     $('#login-form-link').click(function(e) {
@@ -17,5 +31,8 @@ $(function() {
         $(this).addClass('active');
         e.preventDefault();
     });
+
+
+
 
 });
